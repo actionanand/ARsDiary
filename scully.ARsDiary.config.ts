c1ad8843ec18ffc9aa34a38cb721dcafba4777cf
meetanandns <meetanandns@gmail.com>
2021-05-18 01:46:17 -0400

    wsl support files added in config.ts

--- scully.ARsDiary.config.ts
+++ scully.ARsDiary.config.ts
@@ -3,6 +3,22 @@ export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "ARsDiary",
   outDir: './dist/static',
+  puppeteerLaunchOptions: {
+      args: [
+        "--disable-gpu",
+        "--renderer",
+        "--no-sandbox",
+        "--no-service-autorun",
+        "--no-experiments",
+        "--no-default-browser-check",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+        "--disable-extensions"
+      ]
+    },
   routes: {
   }
 };
