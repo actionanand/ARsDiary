85250d60245f4ce132a9d2fbb7bd2cf17b9a80be
meetanandns <meetanandns@gmail.com>
2021-05-29 08:44:11 -0400

    sitemap - removed

--- scully.ARsDiary.config.ts
+++ scully.ARsDiary.config.ts
@@ -1,25 +1,12 @@
-import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { ScullyConfig } from '@scullyio/scully';
 import { WordpressData } from 'scully-wordpress-plugin';
 import { MinifyHtml } from 'scully-plugin-minify-html';
-// import { getDelayAngularPlugin } from '@flowaccount/scully-plugin-angular-delay';
-import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 
-
-const sitemapPlugin = getSitemapPlugin();
 
 // const urlScullyFull = 'https://digital.techiear.com/wp-json/wp/v2/posts?per_page=100';
 const urlScully = 'https://digital.techiear.com/';
 // const urlScully = 'https://wp.learnario.com';
-// const urlSiteMap = 'https://arsdiary.netlify.app';
-const urlSiteMap = 'https://techiear.com/';
 
-setPluginConfig( sitemapPlugin, {
-  urlPrefix: urlSiteMap,
-  sitemapFilename: 'sitemap.xml',
-  merge: false,
-  trailingSlash: false,
-  ignoredRoutes: ['/page-not-found']
-});
 
 const postRenderers = [
   MinifyHtml
